eab7e78e3711bb1a42efa3de98b752b2731ab228
literate <gaekoon@gmail.com>
2024-02-10 17:40:23 +0900

    Feat: 인증 객체를 페이지에 출력한다

--- src/main/java/io/spring/securitysociallogin/controller/HomeController.java
+++ src/main/java/io/spring/securitysociallogin/controller/HomeController.java
@@ -1,12 +1,21 @@
 package io.spring.securitysociallogin.controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
-  @GetMapping("/home")
-  public String home() {
-    return "home";
+  @GetMapping("/api/user")
+  public Authentication user(Authentication authentication, @AuthenticationPrincipal OAuth2User oAuth2User) {
+    return authentication;
+  }
+
+  @GetMapping("/api/oidc")
+  public Authentication oidc(Authentication authentication, @AuthenticationPrincipal OidcUser oidcUser) {
+    return authentication;
   }
 }
