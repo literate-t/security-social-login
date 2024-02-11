93c6f220331ea52ec235a7471801dbee8d42aeb5
literate <gaekoon@gmail.com>
2024-02-11 22:44:48 +0900

    Feat: OAuth2, profile image 추가

--- src/main/java/io/spring/securitysociallogin/model/ProviderUser.java
+++ src/main/java/io/spring/securitysociallogin/model/ProviderUser.java
@@ -3,6 +3,7 @@ package io.spring.securitysociallogin.model;
 import java.util.List;
 import java.util.Map;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 
 public interface ProviderUser {
 
@@ -10,7 +11,14 @@ public interface ProviderUser {
   String getUsername();
   String getPassword();
   String getEmail();
+
   String getProvider();
+
+  String getProfileImage();
+
   List<? extends GrantedAuthority> getAuthorities();
+
+  OAuth2User getOAuth2User();
+
   Map<String, Object> getAttributes();
 }
