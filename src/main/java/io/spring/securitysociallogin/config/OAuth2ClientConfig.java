ef0fe7962956d8e2dcda7ce5ffff343a57717fbe
literate <gaekoon@gmail.com>
2024-02-11 22:43:22 +0900

    Feat: 로그인 예외처리와 form 로그인에 사용되는 customUserDetailsService 추가

--- src/main/java/io/spring/securitysociallogin/config/OAuth2ClientConfig.java
+++ src/main/java/io/spring/securitysociallogin/config/OAuth2ClientConfig.java
@@ -2,28 +2,33 @@ package io.spring.securitysociallogin.config;
 
 import io.spring.securitysociallogin.service.CustomOAuth2UserService;
 import io.spring.securitysociallogin.service.CustomOidcUserService;
+import io.spring.securitysociallogin.service.CustomUserDetailsService;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
 
 @EnableWebSecurity
 public class OAuth2ClientConfig {
 
   private final CustomOAuth2UserService customOAuth2UserService;
   private final CustomOidcUserService customOidcUserService;
+  private final CustomUserDetailsService customUserDetailsService;
 
   public OAuth2ClientConfig(CustomOAuth2UserService customOAuth2UserService,
-      CustomOidcUserService customOidcUserService) {
+      CustomOidcUserService customOidcUserService,
+      CustomUserDetailsService customUserDetailsService) {
     this.customOAuth2UserService = customOAuth2UserService;
     this.customOidcUserService = customOidcUserService;
+    this.customUserDetailsService = customUserDetailsService;
   }
 
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-    http.authorizeRequests().requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll();
+    http.authorizeRequests().requestMatchers(PathRequest.toStaticResources().atCommonLocations())
+        .permitAll();
 
     http.authorizeRequests(
         expressionInterceptUrlRegistry -> expressionInterceptUrlRegistry.antMatchers("/")
@@ -32,11 +37,16 @@ public class OAuth2ClientConfig {
             .antMatchers("/api/oidc").access("hasAnyRole('SCOPE_openid')")
             .anyRequest().authenticated());
 
-    http.oauth2Login(Customizer.withDefaults());
+    http.formLogin().loginPage("/login").loginProcessingUrl("/loginProc").defaultSuccessUrl("/")
+        .permitAll();
+//    http.oauth2Login(Customizer.withDefaults());
     http.oauth2Login(httpSecurityOAuth2LoginConfigurer ->
         httpSecurityOAuth2LoginConfigurer.userInfoEndpoint(userInfoEndpointConfig ->
-            userInfoEndpointConfig.userService(customOAuth2UserService).oidcUserService(customOidcUserService)));
-    http.logout().logoutSuccessUrl("/");
+            userInfoEndpointConfig.userService(customOAuth2UserService)
+                .oidcUserService(customOidcUserService)));
+    http.userDetailsService(customUserDetailsService);
+    http.exceptionHandling()
+        .authenticationEntryPoint(new LoginUrlAuthenticationEntryPoint("/login"));
 
     return http.build();
   }
